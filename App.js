b2828dcc30a3933d530195367c851d59396e0c7f
Jorge <jorge@jaxitank.com>
2019-01-27 21:58:56 -0600

    Centering received message

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { AppRegistry, Text, Dimensions, View } from 'react-native';
 import styles from './styles';
 import Camera from 'react-native-camera';
-import Toast from 'react-native-whc-toast';
+import Toast, {Duration, Position} from 'react-native-whc-toast';
 
 const PicturePath = "";
 const UPLOAD_URL = "http://ec2-52-14-96-102.us-east-2.compute.amazonaws.com/upload";
@@ -19,9 +19,9 @@ export default class Reni extends Component {
             style={styles.preview}
             aspect={Camera.constants.Aspect.fill}
             captureTarget={Camera.constants.CaptureTarget.disk}>
-                    <Toast ref="toast"/>
-                    <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
-                    <Text style={styles.capture} onPress={this.uploadPicture.bind(this)}>[UPLOAD]</Text>                   
+                            <Toast ref="toast"/>
+                            <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+                            <Text style={styles.capture} onPress={this.uploadPicture.bind(this)}>[UPLOAD]</Text>                   
                </Camera>
             </View>
         );
@@ -53,7 +53,7 @@ export default class Reni extends Component {
             fetch(UPLOAD_URL, config)
                 .then((responseData) => {
                     // Show response form the server                    
-                    this.refs.toast.show(responseData.message)
+                    this.refs.toast.showCenter(responseData.message)
                 })
                 .catch(err => {
                     console.log(err);
